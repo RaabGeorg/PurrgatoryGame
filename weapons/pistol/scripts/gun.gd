9113d105e5b7535c071c0549db92a9ff6cc021cf
RaabGeorg <raab.georg02@gmail.com>
2025-11-21 15:41:45 +0100

    changed project structure

--- weapons/pistol/scripts/gun.gd
+++ weapons/pistol/scripts/gun.gd
@@ -5,7 +5,7 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-const bullet = preload("res://bullet.tscn")
+const bullet = preload("res://weapons/bullets/test/scenes/bullet.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
