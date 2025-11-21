58cff0f77a3f47c39579d1d0f5d29fadc3950c25
Stefan427 <stefanauer334@gmail.com>
2025-11-21 14:42:50 +0100

    some bullets

---
+++ node_2d.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+const SPEED: int = 300
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Vector2.RIGHT.rotated(rotation)
+	position += direction * SPEED * delta
