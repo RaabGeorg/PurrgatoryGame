a8b44fda3e6485b46de996a93284e85d1f6fdaf3
RaabGeorg <raab.georg02@gmail.com>
2025-12-25 14:07:43 +0100

    fixed and polished laoding screen

--- entity/menus/scripts/loading_screen.gd
+++ entity/menus/scripts/loading_screen.gd
@@ -3,54 +3,48 @@ extends Control
 var target_scene_path: String
 var progress = []
 var has_finished_loading: bool = false
+var min_time_reached: bool = false
 
 @onready var progress_bar = $ProgressBar
 @onready var status_label = $StatusLabel
 
-func _ready() -> void:
-	#set default
+func _ready():
 	status_label.text = "Loading..."
 	progress_bar.value = 0
 	
-	#check if path is provided
+	#loading bar gets animated artificially (looks cooked otherwise)
+	var tween = create_tween()
+	tween.tween_property(progress_bar, "value", 90.0, 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_OUT)
+	tween.finished.connect(_on_min_time_reached)
+	
 	if target_scene_path != "":
-		#start background loading
-		var error = ResourceLoader.load_threaded_request(target_scene_path)
-		if error != OK:
-			status_label.text = "Error: Could not find scene file."
-	else:
-		status_label.text = "Error: No target scene path set."
-		
+		ResourceLoader.load_threaded_request(target_scene_path)
+
+func _on_min_time_reached():
+	min_time_reached = true
+
 func _process(_delta):
 	if has_finished_loading:
 		return
 
 	var status = ResourceLoader.load_threaded_get_status(target_scene_path, progress)
 	
-	progress_bar.value = progress[0] * 100
-	
-	match status:
-		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
-			pass
-			
-		ResourceLoader.THREAD_LOAD_LOADED:
-			has_finished_loading = true
-			status_label.text = "Press Any Key to Continue"
-			progress_bar.value = 100
-			
-		ResourceLoader.THREAD_LOAD_FAILED:
-			status_label.text = "Error: Loading failed."
+	if status == ResourceLoader.THREAD_LOAD_LOADED and min_time_reached:
+		has_finished_loading = true
+		status_label.text = "Press Any Key to Continue"
 		
-		ResourceLoader.THREAD_LOAD_INVALID_RESOURCE:
-			status_label.text = "Error: Invalid resource path."
+		#letzten prozente reinflanken
+		var final_tween = create_tween()
+		final_tween.tween_property(progress_bar, "value", 100.0, 0.3)
 
 func _input(event):
-	if has_finished_loading:
-		if event is InputEventKey or event is InputEventMouseButton:
-			if event.is_pressed():
-				_switch_to_game()
+	if has_finished_loading and event.is_pressed():
+		_switch_to_game()
 
 func _switch_to_game():
-	var new_scene = ResourceLoader.load_threaded_get(target_scene_path)
+	var packed_scene = ResourceLoader.load_threaded_get(target_scene_path)
+	var map_instance = packed_scene.instantiate()
 	
-	get_tree().change_scene_to_packed(new_scene)
+	get_tree().root.add_child(map_instance)
+	get_tree().current_scene = map_instance
+	self.queue_free()
