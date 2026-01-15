36851db1cd49427bf33e88c38ba2a9e9a5363398
Diart Tahiri <diart.tahiri@hotmail.com>
2026-01-15 20:28:46 +0100

    Added Boss Arena

--- entity/enemies/boss_test/boss_test.gd
+++ entity/enemies/boss_test/boss_test.gd
@@ -7,5 +7,6 @@ func _physics_process(delta: float) -> void:
 	
 
 func _on_health_health_depleted() -> void:
-	get_tree().call_group("Player", "dropped_gold", 666)
+	get_tree().call_group("Player", "dropped_gold", 99)
+	get_tree().call_group("game", "start_timer")
 	queue_free()
--- stages/game.gd
+++ stages/game.gd
@@ -6,7 +6,7 @@ var spawn_count = 1
 var wait_time = 0.75 
 
 @onready var path_follow = get_node("mainCharacter2D/Path2D/PathFollow2D")
-
+@onready var arena = preload("res://stages/boss_arena.tscn").instantiate()
 signal request_open_shop
 
 func _ready() -> void:
@@ -18,19 +18,19 @@ func spawn_mob():
 	var new_mob = preload("res://entity/enemies/test/enemy_test_2d.tscn").instantiate()
 	new_mob.global_position = path_follow.global_position
 	add_child(new_mob)
-
-func boss_spawn_test():
-	var new_mob = preload("res://entity/enemies/boss_test/boss_Test.tscn").instantiate()
-	new_mob.global_position = path_follow.global_position
-
-	add_child(new_mob)
-	
 	
 func spawn_mob_ranged():
 	path_follow.progress_ratio = randf()
 	var new_mob = preload("res://entity/enemies/test/enemy_test_2d_ranged.tscn").instantiate()
 	new_mob.global_position = path_follow.global_position
 	add_child(new_mob)
+
+func boss_spawn_test():
+	var new_mob = preload("res://entity/enemies/boss_test/boss_Test.tscn").instantiate()
+	new_mob.global_position = %mainCharacter2D.global_position + Vector2(0, -100)
+
+	add_child(new_mob)
+
 
 func _on_timer_timeout():
 	for i in range(spawn_count):
@@ -42,9 +42,18 @@ func _on_timer_timeout():
 	if x % 10 == 0:
 		spawn_count += 1
 		
-	if x % 20 == 0:
+	if x % 120 == 0:
+		arena.global_position = %mainCharacter2D.global_position
+		add_child(arena)
+		for n in get_tree().get_nodes_in_group("enemy"):
+			n.queue_free()
+		%WaveTimer.stop()
 		boss_spawn_test()
-	
+		
+func start_timer():
+	arena.queue_free()
+	arena = preload("res://stages/boss_arena.tscn").instantiate()
+	%WaveTimer.start()
 	
 func _process(delta: float) -> void:
 	$mainCharacter2D/Camera2D.enabled = true
